09368e101cc5240d1cbf1e218da6ac7f8905eb33
AmanFuration <aman.maddhesia@furation.tech>
2023-08-28 12:31:59 +0530

    q1 changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,14 @@
+import "./App.css";
+import { Route } from "react-router-dom/cjs/react-router-dom.min";
+import Home from "./pages/Home";
+import Todo from "./pages/Todo";
 
-
-import './App.css';
-import { Route } from 'react-router-dom/cjs/react-router-dom.min';
-import Home from './pages/Home';
-import Todo from './pages/Todo';
-
-
+// Change 2 qw1
 function App() {
   return (
     <div className="App">
-     <Route path="/" component={Home} exact />
-     <Route path="/todo" component={Todo} />
-      
+      <Route path="/" component={Home} exact />
+      <Route path="/todo" component={Todo} />
     </div>
   );
 }
